7cee68e72497210b02bb5d2e1cbadcd91cd9b6b3
Bob Yu-Zhen Huang <cdmyhome3@gmail.com>
2019-11-06 09:05:58 +0000

    complete the project

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const generatePassword = require('./generate_password')
+const app = express()
+const port = 3000
+
+app.engine('handlebars', exphbs({ defaultLayouts: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(express.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.post('/', (req, res) => {
+  const options = req.body
+  const password = generatePassword(options)
+//res.render('index', { password: password, options: options })
+  res.render('index', { password, options }) // in ES6
+})
+
+app.listen(port, () => {
+  console.log(`Express app is listening on port ${port}`)
+})
